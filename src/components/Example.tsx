dc1d0bd9d83ae2577600b6af2dd3a4e05b758f01
Carlos Wimmer <coiote777@gmail.com>
2021-01-03 18:27:13 -0300

    Implement theme.palette example

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Button, Grid, Typography } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 
 const Example: React.FC = () => {
+  const { palette } = useTheme();
+
   return (
     <>
       <Grid container direction="column" justify="center" alignItems="center">
@@ -12,7 +15,10 @@ const Example: React.FC = () => {
           <Typography variant="h3">Central do Servidor</Typography>
         </Grid>
         <Grid item>
-          <Button variant="contained" color="primary">
+          <Button
+            style={{ backgroundColor: palette.warning.main }}
+            variant="contained"
+          >
             Lets Start
           </Button>
         </Grid>
