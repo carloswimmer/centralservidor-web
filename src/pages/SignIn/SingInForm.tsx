10a16ef9360a790c04514a83a9a1773a8d11ad41
Carlos Wimmer <coiote777@gmail.com>
2021-01-13 19:01:13 -0300

    Isolate SignInForm

---
+++ src/pages/SignIn/SingInForm.tsx
@@ -0,0 +1,97 @@
+import React, { useCallback, useState, MouseEvent } from 'react';
+import {
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  makeStyles,
+  TextField,
+  Theme,
+} from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+
+import { useDarkMode } from '../../hooks/darkMode';
+
+const useStyles = makeStyles(({ palette }: Theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      marginTop: 4,
+      marginBottom: 24,
+    },
+    '& .MuiFilledInput-root': {
+      backgroundColor: 'rgba(230, 241, 225, 0.8)',
+      transition: 'background-color 0.3s',
+    },
+    '& .MuiFilledInput-root:hover': {
+      backgroundColor: 'rgba(230, 241, 225, 0.65)',
+    },
+    '& .MuiInputBase-root': {
+      color: palette.text.primary,
+    },
+  },
+  formButton: {
+    marginTop: 8,
+    marginBottom: 56,
+
+    '& button': {
+      height: 48,
+    },
+  },
+}));
+
+const SignInForm: React.FC = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const classes = useStyles();
+  const { darkMode } = useDarkMode();
+
+  const handleClickShowPassword = useCallback(() => {
+    setShowPassword((state) => !state);
+  }, []);
+
+  const handleMouseDownPassword = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+    },
+    [],
+  );
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+        id="sshd"
+        label="SSHD"
+        variant={darkMode ? 'outlined' : 'filled'}
+        fullWidth
+      />
+      <TextField
+        id="password"
+        label="Senha"
+        variant={darkMode ? 'outlined' : 'filled'}
+        type={showPassword ? 'text' : 'password'}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <Visibility /> : <VisibilityOff />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+        error={showPassword}
+        fullWidth
+      />
+      <Box className={classes.formButton}>
+        <Button variant="contained" size="large" color="secondary" fullWidth>
+          Entrar
+        </Button>
+      </Box>
+    </form>
+  );
+};
+
+export default SignInForm;
